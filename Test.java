b276296298af70d77f7beb86649f2ceb95b5807b
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-08-23 23:07:54 +0600

    Learn Object Ref

--- Test.java
+++ Test.java
@@ -1,21 +1,26 @@
 public class Test {
     public static void main(String[] args) {
         Person person = new Person();
-        person.name = "Niloy";
-        person.email = "mushfiqur06@gmail.com";
-        person.phone = "01788689889";
-        person.address = "Dhaka";
-        person.age = 25;
+        // person.name = "Niloy";
+        // person.email = "mushfiqur06@gmail.com";
+        // person.phone = "01788689889";
+        // person.address = "Dhaka";
+        // person.age = 25;
 
-        person.printMe();
+        // person.printMe();
 
         Person person2 = new Person();
-        person2.name = "Mushfiqur Niloy";
-        person2.email = "mushfiqur06@gmail.com";
-        person2.phone = "01788689889";
-        person2.address = "Dhaka";
-        person2.age = 25;
+        Person person3 = person2;
+        Person person4 = person3;
+        // person2.name = "Mushfiqur Niloy";
+        // person2.email = "mushfiqur06@gmail.com";
+        // person2.phone = "01788689889";
+        // person2.address = "Dhaka";
+        // person2.age = 25;
 
-        person2.printMe();
+        // person2.printMe();
+        System.out.println(person2);
+        System.out.println(person4);
+        System.out.println(person3);
     }
 }
